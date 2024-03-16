224dc12bb26c222006c0295d20277908b8ac9e8e
Sellami Mohammed Abdelhadi <philippe.fernandez.2002@gmail.com>
2024-03-16 09:48:32 +0100

    v

--- main/src/Api/APIService.js
+++ main/src/Api/APIService.js
@@ -3,9 +3,10 @@ export default class APIService {
     static recycle(body) {
         return fetch(`https://recycle-garbage-backend.onrender.com/recycle`, {
             'method': 'POST',
-            // headers: {
-            //     'Content-Type': 'application/json'
-            // },
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+                // 'Content-Type': 'application/json'
+            },
             body: body
         })
         .then(response => response.json())
