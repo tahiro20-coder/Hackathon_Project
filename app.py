c8867f3504445cf04fc116d637f8ebe43ad367f8
Sellami Mohammed Abdelhadi <philippe.fernandez.2002@gmail.com>
2024-03-16 11:42:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, send_from_directory,current_app,jsonify,request,render_template
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS #comment this on deployment
-from api.main import Chat,Recycle
+from api.main import Chat
 import os
 import sys
 from api.main import main
@@ -17,6 +17,13 @@ def serve(path):
     print(app.static_folder)
     
     return send_from_directory(app.static_folder,'index.html')
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    # Handle internal server errors and include the CORS header in the response
+    response = jsonify({'error': 'Internal Server Error'})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response, 500
 
 api.add_resource(Chat, '/chat')
 # api.add_resource(Recycle, '/recycle')
