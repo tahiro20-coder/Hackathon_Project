4ab0322b86509a2a342576303f41c424186615a5
Sellami Mohammed Abdelhadi <philippe.fernandez.2002@gmail.com>
2024-05-18 02:14:04 +0200

    Update App.js

--- main/src/App.js
+++ main/src/App.js
@@ -4,11 +4,12 @@ import Header from './Components/Header.jsx';
 import Footer from './Components/Footer.jsx';
 import PageContent from './Components/PageContent.jsx';
 import React from 'react'
-
+import { useTranslation } from "react-i18next";
 
 
 function App() {
-
+  const { t, i18n } = useTranslation();
+  document.body.dir = i18n.dir();
   return (
     <>
     <Header/>
